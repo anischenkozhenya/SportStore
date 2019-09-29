ddf1ef5b7037d040444b4c99e1a1af768966a2dc
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-09-29 23:06:48 +0300

    +234

--- SportStore/Controllers/ProductController.cs
+++ SportStore/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SportStore.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using SportStore.Models.ViewModels;
 namespace SportStore.Controllers
 {
     public class ProductController:Controller
@@ -15,9 +15,25 @@ namespace SportStore.Controllers
         {
             repository = repo;
         }
-        public ViewResult List(int page=1) => View(repository.Products
-            .OrderBy(p=>p.ProductID)
-            .Skip((page-1)*PageSize)
-            .Take(PageSize));
+        public ViewResult List(int page=1) 
+            ////=> View
+            ////(repository.Products
+            => View
+            (new ProductsListViewModel
+            {
+                Products = repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
+        ////.OrderBy(p=>p.ProductID)
+        ////.Skip((page-1)*PageSize)
+        ////.Take(PageSize));
     }
 }
